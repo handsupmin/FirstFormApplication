c5731233550b4f59b673a04ff1fc809b56f2ced8
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-03-08 18:50:39 +0900

    초기 커밋.

---
+++ FirstFormApplication/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace FirstFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(12, 12);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(102, 23);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "한방에";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.myButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "하나씩";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "뉴버튼클릭";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 53);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "0초가 지났습니다";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 346);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button myButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ FirstFormApplication/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApplication
+{
+    public partial class Form1 : Form
+    {
+        int n = 0; // 동적생성을 위한 전역변수1 n
+        int r = 0; // 동적생성을 위한 전역변수2 r
+        int time = 0; // 시간을 카운트하기 위한 전역변수 time
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        { // 누르면 한번에 버튼이 생기도록 하는 이벤트 함수
+            for (int i = 1; i < 5; i++) // 1부터 4까지 반복
+            {
+                Button bt = new Button(); // 버튼 생성 후
+                Controls.Add(bt);
+                bt.Location = new Point(12, 12 + (23 + 3) * i); // 위치 지정
+                bt.Text = "동적 생성 " + i + "번째"; // 버튼의 텍스트 수정
+                bt.Width = 100;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // 누르면 하나씩 아래에 버튼이 생기도록 하는 이벤트 함수
+            n++; // 전역변수 n의 값을 변경
+            Button bt = new Button();
+            Controls.Add(bt);
+            bt.Location = new Point(139, 12 + (23 + 3) * n); // n의 값에따라 y축을 변화시켜줌
+            bt.Text = "동적 생성 " + n + "번째";
+            bt.Width = 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { // 누르면 하나씩 아래에 버튼이 생기고 그 버튼에도 이벤트를 등록하는 함수
+            r++;
+            Button bt = new Button();
+            Controls.Add(bt);
+            bt.Location = new Point(261, 12 + (23 + 3) * r);
+            bt.Text = "동적 생성 " + r + "번째";
+            bt.Width = 100;
+            bt.Click += new System.EventHandler(button2_Click); // 새로만든 버튼에도 생성 이벤트를 추가
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { // 시간이 1초 단위로 지날 때 마다 실행되는 이벤트
+            time++; // 전역변수 time의 값을 1추가
+            button4.Text = time + "초가  지났습니다"; // 버튼의 텍스트 값을 변경
+        }
+    }
+}
